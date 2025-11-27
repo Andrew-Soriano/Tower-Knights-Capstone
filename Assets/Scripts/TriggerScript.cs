6f30d4cbc19068150314631071d0810b72e8285f
Andrew-Soriano <andrew.johnathan.soriano@gmail.com>
2025-11-27 16:28:09 -0500

    Adjusted scenes and prefabs to properly zero out coordinate system.

--- Assets/Scripts/TriggerScript.cs
+++ Assets/Scripts/TriggerScript.cs
@@ -3,17 +3,17 @@ using UnityEngine;
 public class TriggerScript : MonoBehaviour
 {
     public GameObject parent;
-    private CastleController _paranet_controller;
+    private CastleController _parent_controller;
     public int id = 0;
 
     private void Awake()
     {
-        _paranet_controller = parent.GetComponent<CastleController>();
+        _parent_controller = parent.GetComponent<CastleController>();
     }
 
     private void OnTriggerEnter(UnityEngine.Collider other)
     {
         other.gameObject.GetComponent<EnemyController>().trigger(id);
-        _paranet_controller.trigger(id);
+        _parent_controller.trigger(id);
     }
 }
